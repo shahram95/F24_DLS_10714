047e1d18646d76b0e475ac3852ccf04309b0ab9a
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-11 21:05:02 -0400

    Added gradient for Exp

--- hw1/python/needle/ops/ops_mathematic.py
+++ hw1/python/needle/ops/ops_mathematic.py
@@ -311,7 +311,7 @@ class Exp(TensorOp):
 
     def gradient(self, out_grad, node):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return out_grad * exp(node.inputs[0])
         ### END YOUR SOLUTION
 
 
