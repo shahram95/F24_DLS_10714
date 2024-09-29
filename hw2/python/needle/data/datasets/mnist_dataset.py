2b50d6a08c0f5462ca87bf06219b363241e2d53c
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-28 22:20:44 -0400

    Added solution for mnist dataset class

--- hw2/python/needle/data/datasets/mnist_dataset.py
+++ hw2/python/needle/data/datasets/mnist_dataset.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
 from ..data_basic import Dataset
 import numpy as np
+import gzip 
+import struct
 
 class MNISTDataset(Dataset):
     def __init__(
@@ -10,15 +12,49 @@ class MNISTDataset(Dataset):
         transforms: Optional[List] = None,
     ):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        super().__init__(transforms)
+
+        with gzip.open(image_filename, 'rb') as f:
+            magic, num_images, rows, cols = struct.unpack('>IIII', f.read(16))
+            if magic != 2051:
+                    raise ValueError("Invalid magic number in image file")
+            
+            image_data = f.read()
+            images = np.frombuffer(image_data, dtype=np.uint8).reshape(num_images, rows * cols)
+            X = images.astype(np.float32) / 255.0
+
+            # Read labels
+
+        with gzip.open(label_filename, 'rb') as f:
+                magic, num_labels = struct.unpack(">II", f.read(8))
+                if magic != 2049:
+                    raise ValueError("Invalid magic number in label file")
+                
+                label_data = f.read()
+                y = np.frombuffer(label_data, dtype=np.uint8)
+        
+            # Check consistency
+        if num_images != num_labels:
+            raise ValueError(f"Number of images ({num_images}) and labels ({num_labels}) do not match")
+        
+        self.imgs = X
+        self.labels = y
         ### END YOUR SOLUTION
 
     def __getitem__(self, index) -> object:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        image, label = self.imgs[index], self.labels[index]
+
+        if self.transforms:
+            image = image.reshape(28, 28, 1)
+            image = self.apply_transforms(image)
+            image = image.reshape(-1)
+        
+        return image, label
+            
         ### END YOUR SOLUTION
 
     def __len__(self) -> int:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return len(self.imgs)
         ### END YOUR SOLUTION
